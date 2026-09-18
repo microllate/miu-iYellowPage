ed695a994a2bf30c07f22ffe0b84f30caad52ce2
microllate <120251959+microllate@users.noreply.github.com>
2026-09-18 10:27:27 +0800

    fix: align root plugins with version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.ksp) apply false
 }
